92cdd9c454ba99a66cce9eec695feed008affb95
Dr Muhammad Naufal <117550441+drmayu7@users.noreply.github.com>
2025-03-22 13:20:38 +0800

    css styling update

--- app/(logged-out)/page.tsx
+++ app/(logged-out)/page.tsx
@@ -2,10 +2,10 @@ import {Button} from "@/components/ui/button";
 
 export default function LandingPage(){
     return (
-        <div>
-            <h1>Welcome to the landing page!</h1>
+        <>
+            <h1 className="">MyHarmony</h1>
             <Button>Login</Button>
             <Button>Sign Up</Button>
-        </div>
+        </>
     )
 }
