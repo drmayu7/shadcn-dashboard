0c219fdc753e0e8575cd86f7f77a79206ebcb670
Dr Muhammad Naufal <117550441+drmayu7@users.noreply.github.com>
2025-03-22 12:44:14 +0800

    add button component - login & sign up

---
+++ app/(logged-out)/page.tsx
@@ -0,0 +1,11 @@
+import {Button} from "@/components/ui/button";
+
+export default function LandingPage(){
+    return (
+        <div>
+            <h1>Welcome to the landing page!</h1>
+            <Button>Login</Button>
+            <Button>Sign Up</Button>
+        </div>
+    )
+}
