311e51d801d780fd58b2e4e68201bb6b36bd82b8
Dr Muhammad Naufal <117550441+drmayu7@users.noreply.github.com>
2025-03-22 16:08:53 +0800

    add Link to button

--- app/(logged-out)/page.tsx
+++ app/(logged-out)/page.tsx
@@ -1,11 +1,25 @@
-import {Button} from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
+import { BrainIcon } from "lucide-react"; // Icon Library
+import Link from "next/link";
 
 export default function LandingPage(){
     return (
         <>
-            <h1 className="">MyHarmony</h1>
-            <Button>Login</Button>
-            <Button>Sign Up</Button>
+            <h1 className="flex gap-2 items-center">
+                <BrainIcon size={50} className="text-indigo-600"/> MyHarmony
+            </h1>
+            <p>
+                Revolutionizing unstructured data management in MyHDW using Natural Language Processing
+            </p>
+            <div className="flex gap-2 items-center">
+                <Button asChild>
+                    <Link href="/login">Log in</Link>
+                </Button>
+                <small>or</small>
+                <Button asChild variant='outline'>
+                    <Link href="sign-up">Sign Up</Link>
+                </Button>
+            </div>
         </>
     )
 }
