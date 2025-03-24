c1ff9fa50254ad6c7b54e34adfb190c188771c4f
Dr Muhammad Naufal <117550441+drmayu7@users.noreply.github.com>
2025-03-24 22:56:29 +0800

    add sign up form

---
+++ app/(logged-out)/sign-up/page.tsx
@@ -0,0 +1,106 @@
+"use client";
+import { Card, CardDescription, CardHeader, CardTitle, CardContent, CardFooter } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { BrainIcon } from "lucide-react";
+
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
+import {Input} from "@/components/ui/input";
+
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
+
+
+const formSchema = z.object({
+    email: z.string().min(1, { message: "Email is required" }).email({ message: "Invalid email address" }),
+    accountType: z.enum(['MOH', 'Non-MOH']),
+    facilityName: z.string().optional(),
+    numberOfEmployees: z.coerce.number().optional(),
+});
+
+export default function SignupPage(){
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues:{
+            email: ''
+        }
+    });
+
+    const handleSubmit = () => {
+        console.log('login validation passed');
+    }
+
+    return(
+        <>
+            <BrainIcon size={50} />
+            <Card className="w-full max-w-sm">
+                <CardHeader>
+                    <CardTitle>
+                        Sign up
+                    </CardTitle>
+                    <CardDescription>
+                        Sign up for a MyHarmony account
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <Form {...form}>
+                        <form className="flex flex-col gap-4" onSubmit={form.handleSubmit(handleSubmit)}>
+                            <FormField control={form.control}
+                                       name="email"
+                                       render={
+                                            ({field})=> (
+                                           <FormItem>
+                                               <FormLabel>
+                                                   Email
+                                               </FormLabel>
+                                               <FormControl>
+                                                   <Input
+                                                       placeholder='naufal.nordin@moh.gov.my'
+                                                       {...field}/>
+                                               </FormControl>
+                                               <FormMessage />
+                                           </FormItem>
+                                       )}/>
+                            <FormField control={form.control}
+                                       name="accountType"
+                                       render={
+                                           ({field}) => (
+                                               <FormItem>
+                                                   <FormLabel>
+                                                         Account type
+                                                   </FormLabel>
+                                                   <Select onValueChange={field.onChange}>
+                                                        <FormControl>
+                                                            <SelectTrigger className="w-full">
+                                                                <SelectValue placeholder='Select an account type'/>
+                                                            </SelectTrigger>
+                                                        </FormControl>
+                                                       <SelectContent>
+                                                           <SelectItem value='MOH'>Ministry of Health</SelectItem>
+                                                           <SelectItem value='Non-MOH'>Others</SelectItem>
+                                                       </SelectContent>
+                                                   </Select>
+                                                   <FormMessage />
+                                               </FormItem>
+                                           )
+                                       }
+                            />
+                            <Button type="submit">Sign up</Button>
+                        </form>
+                    </Form>
+                </CardContent>
+                <CardFooter className="justify-between">
+                    <small>Already have an account?</small>
+                    <Button asChild variant='outline' size='sm'>
+                        <Link href="/login">
+                            Login
+                        </Link>
+                    </Button>
+                </CardFooter>
+            </Card>
+        </>
+    )
+}
