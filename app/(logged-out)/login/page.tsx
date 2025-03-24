dc170fe69a2f5ca75259825961874fa8e3787479
Dr Muhammad Naufal <117550441+drmayu7@users.noreply.github.com>
2025-03-24 17:11:01 +0800

    add form with zod validation

--- app/(logged-out)/login/page.tsx
+++ app/(logged-out)/login/page.tsx
@@ -4,7 +4,33 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { BrainIcon } from "lucide-react";
 
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
+import {Input} from "@/components/ui/input";
+
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+
+const formSchema = z.object({
+    email: z.string().email(),
+    password: z.string()
+})
+
 export default function LoginPage(){
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues:{
+            email: '',
+            password: '',
+        }
+    });
+
+    const handleSubmit = () => {
+        console.log('login validation passed');
+    }
+
     return(
         <>
                 <BrainIcon size={50} />
@@ -18,7 +44,44 @@ export default function LoginPage(){
                     </CardDescription>
                 </CardHeader>
                 <CardContent>
-                    Login Form
+                    <Form {...form}>
+                        <form className="flex flex-col gap-4" onSubmit={form.handleSubmit(handleSubmit)}>
+                            <FormField control={form.control}
+                                       name="email"
+                                       render={({field})=> (
+                                <FormItem>
+                                    <FormLabel>
+                                        Email
+                                    </FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            placeholder='naufal.nordin@moh.gov.my'
+                                            {...field}/>
+                                    </FormControl>
+                                    <FormDescription>
+                                        Enter your email address that you registered with MyHarmony
+                                    </FormDescription>
+                                    <FormMessage />
+                                </FormItem>
+                                       )}/>
+                            <FormField control={form.control}
+                                       name="password"
+                                       render={({field})=> (
+                                           <FormItem>
+                                               <FormLabel>
+                                                   Password
+                                               </FormLabel>
+                                               <FormControl>
+                                                   <Input type='password'
+                                                          placeholder='Enter your password'
+                                                          {...field}/>
+                                               </FormControl>
+                                               <FormMessage />
+                                           </FormItem>
+                                       )}/>
+                            <Button type="submit">Login</Button>
+                        </form>
+                    </Form>
                 </CardContent>
                 <CardFooter className="justify-between">
                     <small>Don&apos;t have an account?</small>
