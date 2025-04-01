f9db580ea81b124e4d429a1f570b6a3beba48692
Dr Muhammad Naufal <117550441+drmayu7@users.noreply.github.com>
2025-04-01 12:00:49 +0800

    margin left auto avatar

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -8,13 +8,13 @@ export default function DashboardLayout({children}: {children: React.ReactNode})
             <MainMenu />
             <div className="overflow-auto py-2 px-4">
                 <div className="flex gap-2 items-center">
-                    <Avatar className="size-10">
+                    <h1>Welcome back, Dr. Naufal!</h1>
+                    <Avatar className="size-10 ml-auto">
                         <AvatarImage
                             src="https://avatars.githubusercontent.com/u/117550441?u=917a81a96381634b38218c0191ab67970e6776ed&v=4&size=64"
                         />
                         <AvatarFallback>CN</AvatarFallback>
                     </Avatar>
-                    <h1>Welcome back, Dr. Naufal!</h1>
                 </div>
                 {children}
             </div>
