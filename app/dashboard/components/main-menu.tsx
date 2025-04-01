9baf12ac1a37eba19079454dd6fce497fb283a3e
Dr Muhammad Naufal <117550441+drmayu7@users.noreply.github.com>
2025-04-01 09:50:46 +0800

    add avatar on side menu

--- app/dashboard/components/main-menu.tsx
+++ app/dashboard/components/main-menu.tsx
@@ -1,18 +1,26 @@
 import MenuTitle from "./menu-title";
 import MenuItem from "./menu-item";
+import {Avatar, AvatarFallback} from "@/components/ui/avatar";
+import Link from "next/link";
 
 export default function MainMenu(){
     return(
-        <div className="bg-muted overflow-auto p-4">
+        <div className="bg-muted overflow-auto p-4 flex flex-col">
             <div className="border-b dark:border-b-black border-b-zinc-300 pb-4">
                 <MenuTitle />
             </div>
-            <div className="py-4">
+            <div className="py-4 grow">
                 <MenuItem href="/dashboard">My Dashboard</MenuItem>
                 <MenuItem href="/dashboard/teams">Teams</MenuItem>
                 <MenuItem href="/dashboard/employees">Employees</MenuItem>
                 <MenuItem href="/dashboard/account">Account</MenuItem>
                 <MenuItem href="/dashboard/settings">Settings</MenuItem>
+            </div>
+            <div className="flex gap-2 items-center">
+                <Avatar>
+                    <AvatarFallback className="bg-indigo-300 dark:bg-indigo-800">MN</AvatarFallback>
+                </Avatar>
+                <Link href="/">Logout</Link>
             </div>
         </div>
     )
