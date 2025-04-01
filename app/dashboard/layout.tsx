a03bbbc78dc98dafb6435b5ccb756dcf63498dd0
Dr Muhammad Naufal <117550441+drmayu7@users.noreply.github.com>
2025-04-01 09:51:06 +0800

    recentre content page

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -7,14 +7,14 @@ export default function DashboardLayout({children}: {children: React.ReactNode})
         <div className="grid grid-cols-[250px_1fr] h-screen">
             <MainMenu />
             <div className="overflow-auto py-2 px-4">
-                <div className="flex gap-2">
-                    <Avatar className="mt-2 size-10">
+                <div className="flex gap-2 items-center">
+                    <Avatar className="size-10">
                         <AvatarImage
                             src="https://avatars.githubusercontent.com/u/117550441?u=917a81a96381634b38218c0191ab67970e6776ed&v=4&size=64"
                         />
                         <AvatarFallback>CN</AvatarFallback>
                     </Avatar>
-                    <h1 className='pb-4'>Welcome back, Dr. Naufal!</h1>
+                    <h1>Welcome back, Dr. Naufal!</h1>
                 </div>
                 {children}
             </div>
