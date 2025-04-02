3da04b672763742d16c57def8d7aa4b47405450e
Dr Muhammad Naufal <117550441+drmayu7@users.noreply.github.com>
2025-04-02 18:35:16 +0800

    complete card sets

--- app/dashboard/components/staff-stats.tsx
+++ app/dashboard/components/staff-stats.tsx
@@ -1,13 +1,27 @@
-import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
-import {UserIcon} from "lucide-react";
+import {Card, CardContent, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
+import {
+    AlertTriangleIcon,
+    BadgeCheckIcon,
+    PartyPopperIcon,
+    UserCheck2Icon,
+    UserIcon,
+    UserRoundXIcon
+} from "lucide-react";
 import {Button} from "@/components/ui/button";
 import Link from "next/link";
+import Image from "next/image";
+import {Avatar, AvatarFallback} from "@/components/ui/avatar";
+import cm from '@/public/images/cm.jpg'
 
 export default function StaffStats() {
+    const totalEmployees = 426;
+    const staffPresent = 401;
+    const percentageEmployeesPresent = parseFloat(((staffPresent / totalEmployees) * 100).toFixed(2));
+
     return(
         <div className="grid lg:grid-cols-3 gap-4">
-            <Card>
-                <CardHeader>
+            <Card className="gap-0">
+                <CardHeader className="pb-2">
                     <CardTitle className="text-base">
                         Total Staff
                     </CardTitle>
@@ -15,10 +29,10 @@ export default function StaffStats() {
                 <CardContent className="flex justify-between items-center">
                     <div className="flex gap-2">
                         <UserIcon />
-                        <div className="text-5xl font-bold">1000</div>
+                        <div className="text-5xl font-bold">{totalEmployees}</div>
                     </div>
                     <div>
-                        <Button size="sm" asChild>
+                        <Button size="xs" asChild>
                             <Link href="/dashboard/staff">
                                 View All
                             </Link>
@@ -26,19 +40,54 @@ export default function StaffStats() {
                     </div>
                 </CardContent>
             </Card>
-            <Card>
-                <CardHeader>
+            <Card className="gap-0">
+                <CardHeader className="pb-2">
                     <CardTitle className="text-base">
                         Staff Present
                     </CardTitle>
                 </CardHeader>
+                <CardContent>
+                    <div className="flex gap-2">
+                        {percentageEmployeesPresent > 75 ? (
+                            <UserCheck2Icon />) : (<UserRoundXIcon />) }
+
+                        <div className="text-5xl font-bold">{staffPresent}</div>
+                    </div>
+                </CardContent>
+                <CardFooter>
+                    {percentageEmployeesPresent > 75 ? (
+                        <span className="text-xs text-green-500 flex gap-1 items-center">
+                            <BadgeCheckIcon/>
+                                {percentageEmployeesPresent}% of employees are present
+                        </span>
+                    ) : (
+                        <span className="text-xs text-red-500 flex gap-1 items-center">
+                            <AlertTriangleIcon />
+                                Only {percentageEmployeesPresent}% of employees are present
+                        </span>
+                    )}
+
+                </CardFooter>
             </Card>
-            <Card className="border-pink-500">
-                <CardHeader>
+            <Card className="border-pink-500 flex flex-col gap-0">
+                <CardHeader className="pb-2">
                     <CardTitle className="text-base">
                         Staff of the month
                     </CardTitle>
                 </CardHeader>
+                <CardContent className="flex gap-2 items-center">
+                    <Avatar>
+                        <Image src={cm} alt="employee of the month avatar" />
+                        <AvatarFallback>
+                            CM
+                        </AvatarFallback>
+                    </Avatar>
+                    <span className="text-2xl">Collin Murray!</span>
+                </CardContent>
+                <CardFooter className='flex gap-2 items-center text-xs text-muted-foreground mt-auto'>
+                    <PartyPopperIcon className="text-pink-500" />
+                    <span>Congratulations, Colin!</span>
+                </CardFooter>
             </Card>
         </div>
     )
