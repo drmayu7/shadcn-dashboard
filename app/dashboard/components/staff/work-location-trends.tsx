db8448721ad3ec630b80c138b6d88bbc0bcbd9e9
Dr Muhammad Naufal <117550441+drmayu7@users.noreply.github.com>
2025-04-03 15:58:11 +0800

    improve styling of tooltip

--- app/dashboard/components/staff/work-location-trends.tsx
+++ app/dashboard/components/staff/work-location-trends.tsx
@@ -1,25 +1,51 @@
 'use client';
 
 import {data} from "./data"
-import {Bar, BarChart, ResponsiveContainer, Tooltip, XAxis, YAxis} from "recharts";
+import {Bar, BarChart, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis} from "recharts";
 
 export default function WorkLocationTrends() {
     return(
         <ResponsiveContainer width="100%" height={350}>
             <BarChart
                 data={data}
-                margin={{
+                margin={
+                {
                     top: 20,
-                    right: 30,
+                    right: 10,
                     left: 0,
                     bottom: 5,
-                }}
+                }
+            }
+                className="[&_.recharts-tooltip-cursor]:fill-zinc-200 dark:[&_.recharts-tooltip-cursor]:fill-zinc-800"
                 >
-                    <Tooltip />
+                    <Tooltip
+                        separator=": "
+                        formatter={(value, name) => {
+                            switch(name) {
+                                case 'wfh':
+                                    return [value, 'Work from home'];
+                                case 'office':
+                                    return [value, 'Work from office'];
+                                default:
+                                    return [value, name];
+                            }
+                        }}
+                        labelClassName="font-bold text-lg"
+                        wrapperClassName="!text-sm rounded-md dark:!bg-black dark:!border-border"/>
+                    <Legend iconType="circle" verticalAlign="top" align="right"
+                    formatter={
+                        (value)=> {
+                        if (value === "wfh"){
+                            return <span className="text-sm">Work from home</span>
+                    } else if (value === "office") {
+                            return <span className="text-sm">Work from office</span>
+                    }}
+                    }
+                    />
                     <XAxis dataKey="name" stroke="#888888" fontSize={12}/>
                     <YAxis stroke="#888888" fontSize={12} />
-                    <Bar dataKey="office" stackId={1} fill="#4338ca" />
-                    <Bar dataKey="wfh" stackId={1} fill="#6b7280" radius={[4,4,0,0]} />
+                    <Bar dataKey="office" stackId={1} fill="#4f46e5" />
+                    <Bar dataKey="wfh" stackId={1} fill="#a5b4fc" radius={[4,4,0,0]} />
                 </BarChart>
             </ResponsiveContainer>
     )
