83aa817fb2f5a56152c1c985fd7680e5ccb48326
Dr Muhammad Naufal <117550441+drmayu7@users.noreply.github.com>
2025-04-04 10:39:46 +0800

    improve tooltip for line chart

--- app/dashboard/components/teams/support-ticket-resolved.tsx
+++ app/dashboard/components/teams/support-ticket-resolved.tsx
@@ -1,18 +1,24 @@
 "use client";
 
 import {data} from "./data/support-tickets-data"
-import {CartesianGrid, Line, LineChart, ResponsiveContainer, XAxis, YAxis} from "recharts";
+import {CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis} from "recharts";
 
 export default function SupportTicketResolved() {
   return (
    <ResponsiveContainer height={350} width="100%">
        <LineChart data={data}>
+           <Tooltip
+               labelClassName="font-bold text-lg"
+               wrapperClassName="!text-sm rounded-md dark:!bg-black dark:!border-border"/>
            <CartesianGrid strokeDasharray="3" />
            <XAxis fontSize={12} dataKey="name" stroke="#888888"/>
            <YAxis fontSize={12} stroke="#888888"/>
            <Line type="monotone" dataKey="delta" stroke="#84cc16"/>
            <Line type='monotone' dataKey="alpha" stroke="#3b82f6"/>
            <Line type="monotone" dataKey="canary" stroke="#f97316"/>
+           <Legend formatter={(value)=>
+                     <span className="capitalize">{value}</span>
+           }/>
        </LineChart>
 
    </ResponsiveContainer>
