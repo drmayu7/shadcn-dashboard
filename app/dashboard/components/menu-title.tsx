d320aa03cf57d14e60dfb072808dee01acdd4846
Dr Muhammad Naufal <117550441+drmayu7@users.noreply.github.com>
2025-04-07 16:17:21 +0800

    center logo menuTitle if screen <764px

--- app/dashboard/components/menu-title.tsx
+++ app/dashboard/components/menu-title.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function MenuTitle() {
     return(
-        <h4 className="flex items-center gap-2">
+        <h4 className="flex items-center gap-2 justify-center md:justify-start">
             <Link href="/dashboard" className="flex items-center gap-2">
                 <BrainIcon size={40} className="text-primary" />
                 MyHarmony
