14faaf03ef87614f0a01ed196e2d63f4a2582697
Dr Muhammad Naufal <117550441+drmayu7@users.noreply.github.com>
2025-04-08 11:04:22 +0800

    refactor css - justify-center at wrapper instead

--- app/dashboard/components/menu-title.tsx
+++ app/dashboard/components/menu-title.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function MenuTitle() {
     return(
-        <h4 className="flex items-center gap-2 justify-center md:justify-start">
+        <h4 className="flex items-center gap-2">
             <Link href="/dashboard" className="flex items-center gap-2">
                 <BrainIcon size={40} className="text-primary" />
                 MyHarmony
